0221ed2c620de0d68c3ecfcb8bf5459ac03c3e79
Geoffrey Haselden <94574890+Stormray73@users.noreply.github.com>
2026-04-18 02:53:15 +0000

    refactor: migrate middleware to proxy and optimize build configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   allowedDevOrigins: [
     "nextjs--mep--stormray73--snkh7hl1plq0m.pit-1.try.coder.app",
   ],
