09cd228d813819fa8353d11c34a616c8b4e05ea9
stormray73 <geoffreybear1@gmail.com>
2026-05-17 03:16:39 +0000

    fix: neon migration issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const connectionString = process.env.DATABASE_URL;
+
+const neon = new Pool({ connectionString });
+const adapter = new PrismaNeon(neon);
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: ["query"],
   });
 
